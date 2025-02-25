dd75ad09dd291b46aa15a9454b192e9481800034
miguel escola <jubscrebis@gmail.com>
2025-02-25 11:34:59 -0300

    25/02

--- aula-02-arquitetura-mvc-request/app.py
+++ aula-02-arquitetura-mvc-request/app.py
@@ -1,33 +1,12 @@
 # importando o flask e o render
 from flask import Flask, render_template
 
-
+# importando as rotas que estão nos controllers
+from controllers import routes
 # carregando o flask na variável app
 app = Flask(__name__, template_folder='views')
 
-# criando a primeira rota do site
-@app.route('/')
-
-# criando função no site
-def home():
-    # retornando a mensagem de boas vindas
-    return render_template('index.html')
-
-@app.route('/games')
-
-def games():
-    game = {
-        'titulo': 'CS-GO',
-        'ano': '2012',
-        'categoria': 'FPS Online',
-        'jogadores': ['miguel josé', 'miguel isack', 'leaf', 'aspax', 'quemario', 'trop', 'maxxdiego']
-
-    }
-    jogosFriv = ['Fireboy & Watergirl', "Papa's pizzaria", 'Motox3m', 'Bob the Robber', 'Temple Run 2', 'Dynamon']
-    
-    return render_template('games.html', 
-                           game=game, 
-                           jogosFriv = jogosFriv)
+routes.init_app(app)
 
 @app.route('/miguelisack')
 def miguelisack():
